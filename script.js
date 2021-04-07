962347c611e82977729021c9ab1f61c5980c3aeb
shauryajain21 <72522908+shauryajain21@users.noreply.github.com>
2021-04-08 01:17:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
     var timeleft = countDownDate - now;
         
     // Calculating the days, hours, minutes and seconds left
-    var days = Math.floor(timeleft / (1));
+    var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
     var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
